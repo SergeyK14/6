e5248b6e475b01782af755a796a0c9c270a6dd9c
SergeyK <serega_konchakov@mail.ru>
2022-11-29 23:23:49 +0300

    Возведение числа А в степень В

---
+++ Возведение числа А в степень В/Program.cs
@@ -0,0 +1,17 @@
+﻿// Используя определение степени числа, напишите цикл, 
+//который принимает на вход два натуральных числа (A и B) и возводит число A в степень B.
+void StepenAotB()
+{
+    Console.WriteLine("Введите два целых числа, в ином случае возникнет ошибка");
+    Console.WriteLine("Введите число А");
+    int A = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число В");    
+    int B = Convert.ToInt32(Console.ReadLine());
+    if (A >=1 && B >= 1)
+    {
+        double stepen = Math.Pow(A, B);
+        Console.WriteLine("А в степени В = " + stepen);
+    }
+    else Console.WriteLine("Ошибка. Введите натуральное число (>0)");
+}
+StepenAotB();
