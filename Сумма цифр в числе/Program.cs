4742bbfc898b0f042e81a3a031d6c19b07bf20bf
SergeyK <serega_konchakov@mail.ru>
2022-11-29 23:29:51 +0300

    Сумма цифр в числе

---
+++ Сумма цифр в числе/Program.cs
@@ -0,0 +1,15 @@
+﻿// Сумма цифр в числе
+void SummaTsifr()
+{
+Console.WriteLine("Введите число ");
+int chislo = Convert.ToInt32(Console.ReadLine());
+chislo = Math.Abs(chislo);
+int sum = 0;
+while (chislo > 0)
+{
+    sum = sum + chislo%10;
+    chislo = chislo/10;
+}
+Console.WriteLine("Сумма цифр в числе равна " + sum);
+}
+SummaTsifr();
